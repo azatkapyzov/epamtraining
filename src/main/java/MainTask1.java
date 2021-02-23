fe75d6af79595e2950f92622345119b7cc1a7d52
azatkapyzov <kapyzovazat@gmail.com>
2021-02-23 22:24:50 +0600

    Add the 1st task

---
+++ src/main/java/MainTask1.java
@@ -0,0 +1,9 @@
+import java.io.InputStream;
+import java.util.*;
+public class MainTask1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
